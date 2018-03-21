0c8a6d115748c7ee1845173b39b9081e1546a3a6
thach1996 <ngthach96@gmail.com>
2018-03-21 02:52:20 +0000

    Done

--- db/migrate/20180314022310_create_books.rb
+++ db/migrate/20180314022310_create_books.rb
@@ -3,6 +3,7 @@ class CreateBooks < ActiveRecord::Migration
     create_table :books do |t|
       t.string :title
       t.integer :number_of_pages
+      t.string :author
 
       t.timestamps null: false
     end
